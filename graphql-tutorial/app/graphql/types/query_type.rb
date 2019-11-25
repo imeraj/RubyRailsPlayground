1b0231821d8ae94c9473b0722047c5eeddaef262
Meraj <meraj.enigma@gmail.com>
2019-11-24 21:27:15 -0500

    - mutation added with test
    Signed-off-by: Meraj <meraj.enigma@gmail.com>

--- graphql-tutorial/app/graphql/types/query_type.rb
+++ graphql-tutorial/app/graphql/types/query_type.rb
@@ -2,8 +2,7 @@ module Types
   class QueryType < Types::BaseObject
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
-
-    # TODO: remove me
+    
     field :all_links, [LinkType], null: false
 
     def all_links
