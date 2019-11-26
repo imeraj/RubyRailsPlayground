c9d71c9bb23f364b827db685eed1ada986ae5232
Meraj <meraj.enigma@gmail.com>
2019-11-26 18:54:05 -0500

    - modified token generation secret
    Signed-off-by: Meraj <meraj.enigma@gmail.com>

--- graphql-tutorial/app/graphql/mutations/sign_in_user.rb
+++ graphql-tutorial/app/graphql/mutations/sign_in_user.rb
@@ -18,7 +18,7 @@ module Mutations
       return unless user.authenticate(signin[:password])
 
       # use Ruby on Rails - ActiveSupport::MessageEncryptor, to build a token
-      crypt = ActiveSupport::MessageEncryptor.new(Rails.application.credentials.secret_key_base.byteslice(0..31))
+      crypt = ActiveSupport::MessageEncryptor.new(Rails.application.secrets.secret_key_base.byteslice(0..31))
       token = crypt.encrypt_and_sign("user-id:#{ user.id }")
 
       { user: user, token: token }
